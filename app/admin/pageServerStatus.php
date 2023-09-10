1c873da3425ffa5bf57a1e9566aa23287f8a9869
Ahmed Genedy <github@bigprof.com>
2023-09-10 08:35:10 +0000

    23.15.1463 beta

--- app/admin/pageServerStatus.php
+++ app/admin/pageServerStatus.php
@@ -1,6 +1,6 @@
 <?php
-	$appgini_version = '23.15.1457 beta';
-	$generated_ts = '09/09/2023 18:06:40';
+	$appgini_version = '23.15.1463 beta';
+	$generated_ts = '10/09/2023 11:19:20';
 
 	require(__DIR__ . '/incCommon.php');
 
